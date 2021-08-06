502ff7019be73bd751f2beca63a260b286a3b17f
7zxu <jisu7935@g.skku.edu>
2021-08-06 14:43:35 +0900

    Use state instead of variable

--- ReactClass/blog/src/App.js
+++ ReactClass/blog/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
-  let posts = '강남 고기 맛집';
-
-  function func(){
-    return 100
-  }
-  
-  let navStyle = { color:'red', fontSize : '30px' };
-
+  let [ title, titleChange ] = useState(['혼밥 혼영, 혼자 어디까지 해봤니?', '보기만 해도 웃음이 터지는 나만의 웃음 버튼은?','봄이 오면 혹은 비가 오면 꼭 부르고 싶은 애창곡은?']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style = { navStyle }>개발 BLOG</div> 
+        <div>개발 BLOG</div> 
       </div>
-      <img src = { logo } />
-      <h4>{ posts }</h4>
+      <div className = "list">
+        <h3> { title[0] } </h3>
+        <p> 8월 6일 발행</p>
+        <hr/>
+
+        <h3> { title[1] } </h3>
+        <p> 8월 6일 발행</p>
+        <hr/>
+
+        <h3> { title[2] } </h3>
+        <p> 8월 6일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
